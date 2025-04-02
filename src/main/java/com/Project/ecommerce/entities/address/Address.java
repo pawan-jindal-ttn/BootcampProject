1112a3ee11c90165d0b94eb28b32127a169a8c84
pawan-jindal-ttn <pawan.jindal@tothenew.com>
2025-04-02 15:51:25 +0530

    Deleted : Removed useless import statements

--- src/main/java/com/Project/ecommerce/entities/address/Address.java
+++ src/main/java/com/Project/ecommerce/entities/address/Address.java
@@ -1,9 +1,6 @@
 package com.Project.ecommerce.entities.address;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
 
 import java.util.UUID;
